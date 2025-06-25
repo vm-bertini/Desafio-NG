3648d5420b55689f6b4a06d551d66bcd9a0ed761
vm-bertini <victor.m.bertini@gmail.com>
2025-06-24 22:14:46 -0300

    Reworking Class based approach and adding Class folder - MCP_ServerClass first commit too

---
+++ Class/MCP_Server_Class.py
@@ -0,0 +1,34 @@
+from mcp.server.fastmcp import FastMCPServer
+import torch
+from SOM.SOM import SOM   
+
+class MCPServer(FastMCPServer):
+    def __init__(self, name = "MCP Server"):
+        super().__init__()
+        self._server_name = name
+
+    def get_server_name(self):
+        return self._server_name
+    
+    @FastMCPServer.tool()
+    async def Process_Map(self, map, circular: bool = True):
+        #Checking if the map is empty:
+        if not map:
+            return "Map is empty. Please provide a valid map."
+        #Checking if the map is a tensor:
+        if not isinstance(map, torch.Tensor):
+            return "Map must be a tensor. Please provide a valid tensor."
+        
+        #extracting dimensions from the map:
+        input_dim = map.shape[1]
+        dim_1 = map.shape[0]*3
+        dim_2 = 1
+        #Creating SOM instance:
+        som = SOM(dim_1, dim_2, circular, input_dim=input_dim)
+
+        #Training SOM:
+        som.train_som(map, epochs=300)
+        #Getting the final weights:
+        final_weights = som.forward(map)
+        #Returning the final weights:
+        return final_weights.tolist()  # Convert to list for JSON serialization
